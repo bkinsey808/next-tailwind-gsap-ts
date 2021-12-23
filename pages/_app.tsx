dba32082ce25e2dd47476070744196f3fa3e0150
Ben Kinsey <bkinsey@gmail.com>
2021-12-23 11:57:52 -1000

    comments

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
-    // imported like this because GSAP requires body dom element to be present
+    // imported like this because GSAP (or its plugins) requires body dom element to be present
     // see https://greensock.com/forums/topic/22491-gsap3-target-object-not-found/
     import('../helpers/gsap');
   }, []);
